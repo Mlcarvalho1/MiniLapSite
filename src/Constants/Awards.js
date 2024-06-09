6bf958e97d7581f289fc8ce5afa09318d6af627e
Mlcarvalho1 <manoelliradecarvalho@gmail.com>
2024-06-09 20:49:21 -0300

    inverted awards arrays

--- src/Constants/Awards.js
+++ src/Constants/Awards.js
@@ -1,145 +1,144 @@
 const awards = [
-    {
-      date: '1987',
-      title: 'Placa Dourada',
-      subtitle: 'Revista SATUS, instrumento de divulgação oficial da Sociedade de Internos do Hospital de Recife',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '1997',
-      title: 'Membro Titular',
-      subtitle: 'Colégio Brasileiro de Cirurgiões - CBC',
-      description: '',
-      icon: 'member'
-    },
-    {
-      date: '1997',
-      title: 'Placa de Homenagem',
-      subtitle: 'Keio University School of Medicine, Tóquio, Japão',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '1998',
-      title: 'Placa de Homenagem',
-      subtitle: 'IMA - International Medical Association',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '1999',
-      title: 'Placa de Homenagem',
-      subtitle: 'IMA - International Medical Association',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '2000',
-      title: 'International Member',
-      subtitle: 'SAGES - Society of American Gastrointestinal Endoscopic Surgeons',
-      description: '',
-      icon: 'member'
-    },
-    {
-      date: '2002',
-      title: 'Título de Habilitação em Cirurgias Vídeo - Endoscópicas',
-      subtitle: 'SOBRACIL - Sociedade Brasileira de Videocirurgia',
-      description: '',
-      icon: 'title'
-    },
-    {
-      date: '2002',
-      title: 'Presidente',
-      subtitle: 'Sociedade Brasileira de Videocirurgia - SOBRACIL -PE',
-      description: '',
-      icon: 'title'
-    },
-    {
-      date: '2002',
-      title: 'Secretário',
-      subtitle: 'Sociedade Brasileira para o Desenvolvimento da Pesquisa em Cirurgia - SOBRADPEC/PE',
-      description: '',
-      icon: 'title'
-    },
-    {
-      date: '2002',
-      title: 'Placa de Homenagem',
-      subtitle: 'Associação Piauiense de Medicina',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '2003',
-      title: 'Menção Honrosa - Melhor Vídeo do Congresso do CBC',
-      subtitle: 'Colégio Brasileiro de Cirurgiões',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '2005',
-      title: 'POSTER OF DISTINCTION AWARD',
-      subtitle: 'SAGES - Society of American Gastrointestinal Endoscopic Surgeons',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '2005',
-      title: 'Cirurgião Consultor',
-      subtitle: 'Hospital da Restauração - Recife - PE',
-      description: '',
-      icon: 'surgeon'
-    },
-    {
-      date: '2005',
-      title: 'Segundo melhor vídeo do Congresso do CBC',
-      subtitle: 'Colégio Brasileiro de Cirurgiões',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '2006',
-      title: 'Perpetual Member',
-      subtitle: 'ELSA - Endoscopic and Laparoscopic Surgeons of Asia',
-      description: '',
-      icon: 'member'
-    },
-    {
-      date: '2006',
-      title: 'Interesting Case Report',
-      subtitle: 'SAGES - Society of American Gastrointestinal Endoscopic Surgeons',
-      description: '',
-      icon: 'award'
-    },
-    {
-      date: '2007',
-      title: 'BEST POSTER OF THE MEETING',
-      subtitle: 'EAES - 15th EAES International Congress',
-      description: 'Breakthrough on Technique for More Secure, Quicker, and Less Expensive Placement and Removal of Intragastric Balloons',
-      icon: 'award'
-    },
-    {
-      date: '2008',
-      title: 'BEST POSTER OF THE SECTION IN THE 2008 WCES MEETING',
-      subtitle: 'World Congress of Endoscopic Surgery',
-      description: 'LAPAROSCOPIC-ASSISTED TRANSVAGINAL SPLENECTOMY - CASE REPORT.',
-      icon: 'award'
-    },
-    {
-      date: '2016',
-      title: 'Honorary Chair of The Society of Laparoendoscopic Surgeons',
-      subtitle: 'Society of Laparoendoscopic Surgeons - SLS',
-      description: '',
-      icon: 'title'
-    },
-    {
+  {
       date: '2022',
       title: 'EXCEL AWARD',
       subtitle: 'SLS - Society of Laparoscopic & Robotic Surgeons',
       description: '',
       icon: 'award'
-    }
+  },
+  {
+      date: '2016',
+      title: 'Honorary Chair of The Society of Laparoendoscopic Surgeons',
+      subtitle: 'Society of Laparoendoscopic Surgeons - SLS',
+      description: '',
+      icon: 'title'
+  },
+  {
+      date: '2008',
+      title: 'BEST POSTER OF THE SECTION IN THE 2008 WCES MEETING',
+      subtitle: 'World Congress of Endoscopic Surgery',
+      description: 'LAPAROSCOPIC-ASSISTED TRANSVAGINAL SPLENECTOMY - CASE REPORT.',
+      icon: 'award'
+  },
+  {
+      date: '2007',
+      title: 'BEST POSTER OF THE MEETING',
+      subtitle: 'EAES - 15th EAES International Congress',
+      description: 'Breakthrough on Technique for More Secure, Quicker, and Less Expensive Placement and Removal of Intragastric Balloons',
+      icon: 'award'
+  },
+  {
+      date: '2006',
+      title: 'Interesting Case Report',
+      subtitle: 'SAGES - Society of American Gastrointestinal Endoscopic Surgeons',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '2006',
+      title: 'Perpetual Member',
+      subtitle: 'ELSA - Endoscopic and Laparoscopic Surgeons of Asia',
+      description: '',
+      icon: 'member'
+  },
+  {
+      date: '2005',
+      title: 'Segundo melhor vídeo do Congresso do CBC',
+      subtitle: 'Colégio Brasileiro de Cirurgiões',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '2005',
+      title: 'Cirurgião Consultor',
+      subtitle: 'Hospital da Restauração - Recife - PE',
+      description: '',
+      icon: 'surgeon'
+  },
+  {
+      date: '2005',
+      title: 'POSTER OF DISTINCTION AWARD',
+      subtitle: 'SAGES - Society of American Gastrointestinal Endoscopic Surgeons',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '2003',
+      title: 'Menção Honrosa - Melhor Vídeo do Congresso do CBC',
+      subtitle: 'Colégio Brasileiro de Cirurgiões',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '2002',
+      title: 'Placa de Homenagem',
+      subtitle: 'Associação Piauiense de Medicina',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '2002',
+      title: 'Secretário',
+      subtitle: 'Sociedade Brasileira para o Desenvolvimento da Pesquisa em Cirurgia - SOBRADPEC/PE',
+      description: '',
+      icon: 'title'
+  },
+  {
+      date: '2002',
+      title: 'Presidente',
+      subtitle: 'Sociedade Brasileira de Videocirurgia - SOBRACIL -PE',
+      description: '',
+      icon: 'title'
+  },
+  {
+      date: '2002',
+      title: 'Título de Habilitação em Cirurgias Vídeo - Endoscópicas',
+      subtitle: 'SOBRACIL - Sociedade Brasileira de Videocirurgia',
+      description: '',
+      icon: 'title'
+  },
+  {
+      date: '2000',
+      title: 'International Member',
+      subtitle: 'SAGES - Society of American Gastrointestinal Endoscopic Surgeons',
+      description: '',
+      icon: 'member'
+  },
+  {
+      date: '1999',
+      title: 'Placa de Homenagem',
+      subtitle: 'IMA - International Medical Association',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '1998',
+      title: 'Placa de Homenagem',
+      subtitle: 'IMA - International Medical Association',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '1997',
+      title: 'Placa de Homenagem',
+      subtitle: 'Keio University School of Medicine, Tóquio, Japão',
+      description: '',
+      icon: 'award'
+  },
+  {
+      date: '1997',
+      title: 'Membro Titular',
+      subtitle: 'Colégio Brasileiro de Cirurgiões - CBC',
+      description: '',
+      icon: 'member'
+  },
+  {
+      date: '1987',
+      title: 'Placa Dourada',
+      subtitle: 'Revista SATUS, instrumento de divulgação oficial da Sociedade de Internos do Hospital de Recife',
+      description: '',
+      icon: 'award'
+  }
 ];
-    
-  export default awards;
-  
+
+export default awards;
